f8d2d911e0cd62510d81fceb6af86a9e2a9e50d8
Cai Jiaxiu <jiaxiunov@gmail.com>
2020-10-18 13:41:23 +0800

    Add commenting to line graph notes

--- vue-client/src/views/AutoEvaluation.vue
+++ vue-client/src/views/AutoEvaluation.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-    <div>
-    <Plotly :data="data" :layout="layout" :display-mode-bar="false" :static-plot="true" :responsive="true"></Plotly>
-    <Plotly v-for="graph in graphs" :data="graph.data" :layout="graph.layout" :display-mode-bar="false" :static-plot="true" :responsive="true"></Plotly>
-    </div>
+    v-card.mx-10
+        Plotly(v-for="graph in graphs" :data="graph.data" :layout="graph.layout" :display-mode-bar="false" :responsive="true" @hover="onHover" @unhover="onUnhover" @click="onClick")
+        v-card-title.mt-10 Select a note to leave comment
+        v-card-subtitle(v-if="selectedNote") Selected: {{selectedNote.note}}
+        v-text-field.mx-10(label="comment" hint="comment on a note" persistent-hint outlined :disabled="selectedNote == null" @change="onCommentChange" v-model="comment")
 </template>
 
 <script>
@@ -13,6 +14,28 @@ export default {
     Plotly
   },
   methods: {
+    onCommentChange(data) {
+      let selectedNote = { ...this.selectedNote };
+      selectedNote.comment = data;
+      this.transcribedNotes[this.selectedIndex] = selectedNote;
+      this.graphs = this.getGraphsForNotes(this.transcribedNotes);
+    },
+    onClick(data) {
+      let idx = data.points[0].pointIndex;
+      if (this.selectedIndex !== idx) {
+        this.selectedIndex = idx;
+        this.selectedNote = this.transcribedNotes[idx];
+        this.comment = null;
+      }
+    },
+    onHover(data) {
+      let idx = data.points[0].pointIndex;
+      console.log(idx);
+    },
+    onUnhover(data) {
+      let idx = data.points[0].pointIndex;
+      console.log(idx);
+    },
     getSecondsPerRow() {
       let timeSignature = 4;
       let bpm = 60;
@@ -44,42 +67,20 @@ export default {
     getLineSegmentYValForMusicNote(note) {
       let minY = this.getNumberFromMusicNote(this.minNoteNumber);
       let maxY = this.getNumberFromMusicNote(this.maxNoteNumber);
-      let y = this.getNumberFromMusicNote(note.noteNumber);
+      let y = this.getNumberFromMusicNote(note.note);
       return (
         ((y - minY) / (maxY - minY)) *
         (this.maxNoteNumber[1] - this.minNoteNumber[1])
       );
     },
-    splitIntoRows(notes) {
-      let timePerRow = this.getSecondsPerRow();
-      let newRowStartTime = 0;
-      let rows = [];
-      rows.push([]);
-      for (let i = 0; i < notes.length; i++) {
-        let note = notes[i];
-        if (note.onset < newRowStartTime + timePerRow) {
-          if (note.onset + note.duration <= newRowStartTime + timePerRow) {
-            rows[rows.length - 1].push(note);
-          } else {
-            // split a note that lasts across two rows into two notes
-            let noteCopy = { ...note };
-            noteCopy.onset = newRowStartTime + timePerRow;
-            noteCopy.duration =
-              note.onset + note.duration - (newRowStartTime + timePerRow);
-            note.duration = note.duration - noteCopy.duration;
-            rows[rows.length - 1].push(note);
-            notes[i] = noteCopy;
-            newRowStartTime = newRowStartTime + timePerRow;
-            rows.push([]);
-            i--;
-          }
-        } else {
-          newRowStartTime = newRowStartTime + timePerRow;
-          rows.push([]);
-          i--;
-        }
+    getTickTextsForRow(rowIndex) {
+      let ticks = [];
+      for (let i = 0; i <= this.barsPerRow * this.timeSignature; i++) {
+        ticks.push(i);
       }
-      return rows;
+      return ticks.map(tick =>
+        (tick + rowIndex * this.barsPerRow * this.timeSignature).toString()
+      );
     },
     getTickValsForRow(rowIndex) {
       let ticks = [];
@@ -87,7 +88,7 @@ export default {
         ticks.push(i);
       }
       return ticks.map(
-        tick => tick + rowIndex * this.barsPerRow * this.timeSignature
+        tick => (tick * 60) / this.bpm + rowIndex * this.secondsPerRow
       );
     },
     getAnnotationXValsForNotes(notes) {
@@ -102,7 +103,14 @@ export default {
       });
     },
     getAnnotationsForNotes(notes) {
-      return notes.map(note => note.noteNumber);
+      let annotations = [];
+      for (let i = 0; i < notes.length; i++) {
+        let comment = notes[i].comment;
+        let note = notes[i].note;
+        let annotation = comment ? note + " " + comment : note;
+        annotations.push(annotation);
+      }
+      return annotations;
     },
     getBarDividers(rowIndex) {
       let y0 = 0;
@@ -110,8 +118,8 @@ export default {
       let barDividers = [];
       for (let i = 1; i <= this.barsPerRow; i++) {
         let x =
-          i * this.timeSignature +
-          rowIndex * this.timeSignature * this.barsPerRow;
+          (i * this.timeSignature * 60) / this.bpm +
+          rowIndex * this.secondsPerRow;
         let divider = {
           type: "line",
           x0: x,
@@ -127,176 +135,118 @@ export default {
       }
       return barDividers;
     },
+    getBarXValsForNotes(notes) {
+      return notes.map(note => note.onset + note.duration / 2);
+    },
+    getBarYValsForNotes(notes) {
+      return Array(notes.length).fill(0.1);
+    },
+    getBarBaseValsForNotes(notes) {
+      return notes.map(note => {
+        return this.getLineSegmentYValForMusicNote(note) - 0.05;
+      });
+    },
+    getBarWidthValsForNotes(notes) {
+      return notes.map(note => note.duration);
+    },
+    getBarColorsForNotes(notes) {
+      return Array(notes.length).fill("rgb(55, 128, 191)");
+    },
     getGraphsForNotes(notes) {
-      let rows = this.splitIntoRows(notes);
-      let graphs = [];
-      for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let graph = {
-          data: [
-            {
-              x: this.getAnnotationXValsForNotes(row),
-              y: this.getAnnotationYValsForNotes(row),
-              text: this.getAnnotationsForNotes(row),
-              mode: "text"
-            }
-          ],
-          layout: {
-            xaxis: {
-              zeroline: false,
-              range: [
-                i * this.secondsPerRow,
-                (i + 1) * this.secondsPerRow + 0.1
-              ],
-              tickvals: this.getTickValsForRow(i),
-              tickfont: {
-                family: "Old Standard TT, serif",
-                size: 10,
-                color: "Brown"
-              }
+      let row = notes;
+      let graph = {
+        data: [
+          {
+            type: "bar",
+            x: this.getBarXValsForNotes(row),
+            y: this.getBarYValsForNotes(row),
+            width: this.getBarWidthValsForNotes(row),
+            base: this.getBarBaseValsForNotes(row),
+            text: this.getAnnotationsForNotes(row),
+            hovertemplate: "<i>%{text}</i><extra></extra>",
+            marker: {
+              color: this.getBarColorsForNotes(row)
             },
-            yaxis: {
-              range: [0, 3.1],
-              tickvals: [0, 1, 2, 3],
-              ticktext: ["C3", "C4", "C5", "C6"],
-              tickfont: {
-                family: "Old Standard TT, serif",
-                size: 10,
-                color: "Brown"
-              }
-            },
-            width: 1200,
-            height: 250,
-            shapes: row
-              .map(note => this.getLineSegmentForMusicNote(note))
-              .concat(this.getBarDividers(i))
+            name: "notes"
           }
-        };
-        graphs.push(graph);
-      }
-      return graphs;
+        ],
+        layout: {
+          hovermode: "closest",
+          hoverlabel: { bgcolor: "#FFF" },
+          xaxis: {
+            zeroline: false,
+            range: [
+              (this.rowNum - 1) * this.secondsPerRow,
+              this.rowNum * this.secondsPerRow + 0.1
+            ],
+            tickvals: this.getTickValsForRow(this.rowNum - 1),
+            ticktext: this.getTickTextsForRow(this.rowNum - 1),
+            tickfont: {
+              family: "Old Standard TT, serif",
+              size: 10,
+              color: "Brown"
+            }
+          },
+          yaxis: {
+            range: [0, 3.3],
+            tickvals: [0, 1, 2, 3],
+            ticktext: ["C3", "C4", "C5", "C6"],
+            tickfont: {
+              family: "Old Standard TT, serif",
+              size: 10,
+              color: "Brown"
+            }
+          },
+          height: 150,
+          margin: {
+            l: 30,
+            r: 20,
+            b: 20,
+            t: 20,
+            pad: 8
+          },
+          shapes: this.getBarDividers(this.rowNum - 1)
+        }
+      };
+      return [graph];
     }
   },
 
   data() {
     return {
+      comment: null,
+      selectedNote: null,
+      selectedIndex: null,
       minNoteNumber: "C3",
       maxNoteNumber: "C6",
       bpm: 60,
+      rowNum: 1,
       timeSignature: 4,
       barsPerRow: 4,
       secondsPerRow: this.getSecondsPerRow(),
       transcribedNotes: [
         {
           onset: 0.3,
-          noteNumber: "C6",
+          note: "C6",
           duration: 5
         },
         {
           onset: 5.3,
-          noteNumber: "A3",
+          note: "A3",
           duration: 4
         },
         {
           onset: 10,
-          noteNumber: "D3",
+          note: "D3",
           duration: 8
-        },
-        {
-          onset: 18,
-          noteNumber: "E3",
-          duration: 5
-        },
-        {
-          onset: 23,
-          noteNumber: "F3",
-          duration: 4
-        },
-        {
-          onset: 28,
-          noteNumber: "G3",
-          duration: 5
-        },
-        {
-          onset: 33,
-          noteNumber: "A3",
-          duration: 3
         }
       ],
-      graphs: [],
-      data: [
-        {
-          x: [0.75, 1.8],
-          y: [0.2, 0.6],
-          text: ["A1", "A2"],
-          mode: "text"
-        }
-      ],
-      layout: {
-        title: "My graph",
-        xaxis: {
-          range: [0, 4]
-        },
-        yaxis: {
-          range: [0, 3],
-          tickvals: [0, 1, 2, 3],
-          ticktext: ["C3", "C4", "C5", "C6"]
-        },
-        width: 400,
-        height: 250,
-        shapes: [
-          {
-            type: "line",
-            x0: 0.5,
-            y0: 0.5,
-            x1: 1,
-            y1: 0.5,
-            line: {
-              color: "rgb(55, 128, 191)",
-              width: 3
-            }
-          },
-          {
-            type: "line",
-            x0: 1.5,
-            y0: 1,
-            x1: 2.2,
-            y1: 1,
-            line: {
-              color: "rgb(127, 255, 212)",
-              width: 3
-            }
-          },
-          {
-            type: "line",
-            x0: 2.2,
-            y0: 1.5,
-            x1: 3,
-            y1: 1.5,
-            line: {
-              color: "rgb(255, 127, 80)",
-              width: 3
-            },
-            name: "A4"
-          },
-          {
-            type: "line",
-            x0: 3,
-            y0: 1,
-            x1: 3.3,
-            y1: 1,
-            line: {
-              color: "rgb(255, 215, 0)",
-              width: 3
-            }
-          }
-        ]
-      }
+      graphs: []
     };
   },
-  created() {
+  mounted() {
     this.graphs = this.getGraphsForNotes(this.transcribedNotes);
-  },
+  }
 };
 </script>
 
