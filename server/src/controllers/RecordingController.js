b3b0167a14dca04079dee83ea7c1e7d2d05f89f2
niclausliu <niclausliu>
2020-10-26 15:55:18 +0800

    Add audio file conversion (to ogg) to backend

--- server/src/controllers/RecordingController.js
+++ server/src/controllers/RecordingController.js
@@ -7,6 +7,9 @@ const AWS = require("aws-sdk");
 const config = require("../config/config");
 const { sequelize } = require("../models");
 const transcribe = require("../transcription/transcribe");
+const path = require('path')
+const ffmpeg = require('fluent-ffmpeg')
+const fs = require('fs');
 
 const s3 = new AWS.S3({
   accessKeyId: config.aws.id,
@@ -42,15 +45,65 @@ module.exports = {
         }
 
         console.log(req.file)
+        let fileBuffer, filePath, outputPath;
+        let outputName = req.file.originalname;
         if (req.file) {
+          // Convert non-wav file into wav file.
+          if (req.file.originalname.slice(-4) !== '.wav') {
+            const dir = './temp';
+            filePath = `${dir}/original_${req.file.originalname}`;
+
+            // Change file extension.
+            const strings = req.file.originalname.split('.');
+            outputName = `${strings.slice(0, strings.length == 1 ? 1 : -1)}.wav`;
+            outputPath = `${dir}/${outputName}`;
+
+            if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir);
+            }
+            fs.writeFileSync(filePath, req.file.buffer);
+
+            await new Promise((resolve, reject) => {
+              ffmpeg(filePath)
+                .noVideo()
+                .toFormat('wav')
+                .output(outputPath)
+                .on('progress', (progress) => {
+                  console.log(progress)
+                })
+                .on('error', (err) => {
+                  console.log(`[ffmpeg] error: ${err.message}`)
+                  reject(err)
+                })
+                .on('end', async () => {
+                  console.log('[ffmpeg] finished')
+                  resolve()
+                })
+                .run()
+            })
+
+            fileBuffer = fs.readFileSync(outputPath);
+          } else {
+            fileBuffer = req.file.buffer;
+          }
+
+          // Uploading audio file to the bucket.
           let params = {
             Bucket: config.aws.bucket,
-            Key: `${user.email}/${exercise.Lesson.Course.dataValues.name}/${exercise.Lesson.name}/${exercise.name}/audio/${req.file.originalname}`,
-            Body: req.file.buffer,
+            Key: `${user.email}/${exercise.Lesson.Course.dataValues.name}/${exercise.Lesson.name}/${exercise.name}/audio/${outputName}`,
+            Body: fileBuffer,
           };
 
-          // Uploading audio file to the bucket
           const response = await s3.upload(params).promise();
+
+          // Remove temp files.
+          if (filePath) {
+            fs.unlinkSync(filePath);
+          }
+          if (outputPath) {
+            fs.unlinkSync(outputPath);
+          }
+
           req.body.UserId = user.id
           req.body.audioUrl = response.Location;
           req.body.audioFilename = req.file.originalname;
@@ -278,6 +331,7 @@ module.exports = {
           [sequelize.col('exercise.id'), 'exercise_id'],
           [sequelize.col('exercise.lesson.id'), 'lesson_id'],
           [sequelize.col('exercise.lesson.course.id'), 'course_id'],
+          [sequelize.col('exercise.lesson.course.name'), 'course_name'],
           [sequelize.col('user.username'), 'student_name'],
           [sequelize.col('exercise.lesson.course.tutor.username'), 'tutor_name'],
         ],
