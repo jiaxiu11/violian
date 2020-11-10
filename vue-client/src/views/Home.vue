5fb0a6ded82d157623844d631d77a3751cae01f6
R-D-D-D <wangrunding@gmail.com>
2020-11-10 20:23:39 +0800

    add video

--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -1,12 +1,9 @@
 <template lang="pug">
   div
     v-container
-      v-row.mt-10
-        v-col.text-left(cols="8")
-          h1 Welcome to Violian!
-
-      v-row.mb-12
-        v-col(cols="8")
+      v-row.mt-16.mb-12(justify="space-between")
+        v-col.text-left(cols="12" md="6")
+          h1.pb-6 Welcome to Violian!
           v-autocomplete(v-model='model' :items='items' :loading='isLoading' :search-input.sync='search' cache-items multiple clearable hide-details hide-selected item-text='name' item-value='symbol' label='Search for a course...' solo)
             template(v-slot:no-data)
               v-list-item
@@ -20,6 +17,9 @@
             template(v-slot:item='{ item }')
               v-list-item(@click="go_to_course($event, item.id)")
                 v-list-item-content {{ item.name }}
+
+        v-col.text-left(cols="4" v-if="cardsPerRow > 2")
+          video.vjs-big-play-centered(ref="videoPlayer" class="video-js" :id="`video`")
 
       v-divider
 
@@ -63,11 +63,13 @@
 </template>
 
 <script>
+/* eslint-disable */
 import { mapState } from 'vuex'
 import CourseService from '@/services/CourseService'
 import SubscriptionService from '@/services/SubscriptionService'
 import _ from 'lodash'
 import CourseCard from '@/components/Course/CourseCard'
+import videojs from "video.js";
 
 export default {
   name: 'Home',
@@ -177,6 +179,27 @@ export default {
         this.$set(this.coursesInCategory, this.popularCourses[i].instrument, this.coursesInCategory[this.popularCourses[i].instrument].concat(this.popularCourses[i]))
       }
     }
+
+    const player = videojs(this.$refs.videoPlayer, {
+        controls: true,
+        aspectRatio: '16:9',
+        sources: [
+          {
+            src: 'https://rhythm-academy.s3-ap-southeast-1.amazonaws.com/violian.mp4',
+            type: "video/mp4"
+          }
+        ],
+        playbackRates: [0.8, 0.9, 1, 1.1, 1.2],
+        poster: 'https://rhythm-academy.s3-ap-southeast-1.amazonaws.com/violian_cover.jpg'
+      }, () => {
+        // console.log('onPlayerReady', this)
+        player.hotkeys({
+          volumeStep: 0.1,
+          seekStep: 2,
+          enableModifiersForNumbers: false,
+          enableHoverScroll: true
+        })
+    })
   }
 }
 </script>
